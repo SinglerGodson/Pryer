de541f2b3c7f70845c856eb9211aee8713c8a89b
maenfang1 <maenfang1@jd.com>
2019-03-15 23:29:41 +0800

    优化代码，实现支持所有类型文件，及常用注释标记的解析。

--- src/com/jd/n2/pryer/decorator/PryerDecorator.java
+++ src/com/jd/n2/pryer/decorator/PryerDecorator.java
@@ -7,10 +7,12 @@ import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.packageDependencies.ui.PackageDependenciesNode;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
 import com.intellij.psi.javadoc.PsiDocComment;
 import com.intellij.psi.javadoc.PsiDocTag;
 import com.intellij.psi.javadoc.PsiDocTagValue;
 import com.intellij.ui.ColoredTreeCellRenderer;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * 类注释追加 装饰器
@@ -27,31 +29,22 @@ public class PryerDecorator implements ProjectViewNodeDecorator {
     public static final int COMMENT_MAX_LENGTH = 20;
     /** 要显示的标签名称 **/
     public static final String TAG_NAMES = "description";
+    /** 常用注释标记 **/
+    private static final String[] COMMENT_TAGS = new String[]{ "<!--", "/**", "#"};
 
     @Override
     public void decorate(ProjectViewNode node, PresentationData data) {
+        String comment = null;
         Object value = node.getValue();
-        if (value instanceof PsiClass) {
+        if (value instanceof PsiClass) { // 若是javaClass文件
             PsiClass psiClass = (PsiClass) value;
-            PsiDocComment docComment = psiClass.getDocComment();
-            if (docComment != null) {
-                PsiDocTag tag = findTag(docComment);
-                if (tag != null) { // 读取tag标签的注释内容
-                    PsiDocTagValue tagValue = tag.getValueElement();
-                    if (tagValue != null) {
-                        setLocationString(data, tagValue.getText());
-                    }
-                } else { // 如果没有找到tag，直接读取第一行注释内容
-                    PsiElement[] elements = docComment.getDescriptionElements();
-                    for(PsiElement e : elements){
-                        String comment = e.getText().trim().replace("\n", "");
-                        if(!StringUtil.isEmpty(comment)){
-                            setLocationString(data, e.getText());
-                            break;
-                        }
-                    }
-                }
-            }
+            comment = getClassComment(psiClass);
+        } else if(value instanceof PsiFile){ // 若是其他类型文件
+            PsiFile psiFile = (PsiFile) value;
+            comment = getFileComment(psiFile);
+        }
+        if(StringUtils.isNotEmpty(comment)){
+            setLocationString(data, comment);
         }
     }
 
@@ -87,6 +80,81 @@ public class PryerDecorator implements ProjectViewNodeDecorator {
             PsiDocTag tag = docComment.findTagByName(tagName);
             if(tag != null){
                 return tag;
+            }
+        }
+        return null;
+    }
+
+
+     /**
+      * 获取java类文件注释
+      * @author maenfang1
+      * @date 2019/3/15 21:31
+      * @param
+      * @return
+      */
+    private String getClassComment(PsiClass psiClass){
+        PsiDocComment docComment = psiClass.getDocComment();
+        if (docComment != null) {
+            PsiDocTag tag = findTag(docComment);
+            if (tag != null) { // 读取tag标签的注释内容
+                PsiDocTagValue tagValue = tag.getValueElement();
+                if (tagValue != null) {
+                    return tagValue.getText();
+                }
+            } else { // 如果没有找到tag，直接读取第一行注释内容
+                PsiElement[] elements = docComment.getDescriptionElements();
+                for(PsiElement e : elements){
+                    String comment = e.getText().trim().replace("\n", "");
+                    if(!StringUtil.isEmpty(comment)){
+                        return comment;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+
+    /**
+     * 获取其他类型文件的注释内容
+     * @author maenfang1
+     * @date 2019/3/15 22:59
+     * @param
+     * @return
+     */
+    private String getFileComment(PsiFile psiFile){
+        for(String tag : COMMENT_TAGS){
+            String comment = getComment(tag, psiFile.getText());
+            if(StringUtils.isNotEmpty(comment)){
+                return comment;
+            }
+        }
+        return null;
+    }
+
+     /**
+      * 获取 <!-- 注释 与 /** 注释 的内容
+      * @author maenfang1
+      * @date 2019/3/15 22:45
+      * @param
+      * @return
+      */
+    private String getComment(String commentTag, String text){
+        // 如果不是以指定注释符开始，则不显示注释内容。
+        if(!text.startsWith(commentTag)){
+            return null;
+        }
+        String[] lines = text.split("\n");
+        for(String line : lines){
+            if(line.trim().startsWith(commentTag)){
+                line = line.replace(commentTag, "");
+            }
+            if(!StringUtils.isEmpty(line)){
+                return line
+                        .replace("*/", "")
+                        .replace("*", "")
+                        .replace("-->", "").trim();
             }
         }
         return null;
